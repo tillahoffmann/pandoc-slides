4e4a059d768500ce620e5a2403ae0f75466c028f
Till Hoffmann <tillahoffmann@gmail.com>
2022-12-12 18:36:41 -0500

    Use single instance rather than singleton and update code lens handling.

--- src/extension.ts
+++ src/extension.ts
@@ -8,54 +8,55 @@ import * as child_process from 'child_process';
 import * as tmp from 'tmp-promise';
 import * as uuid from 'uuid';
 import { promisify } from 'util';
+import { setFlagsFromString } from 'v8';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pandoc-slides.sidePreview', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		showSidePreview(context);
-	});
-	context.subscriptions.push(disposable);
-
-	// Set up the code lens for navigating to slides.
-	disposable = vscode.languages.registerCodeLensProvider({ scheme: "file", language: "markdown"},
-											  			   new CodeLensProvider());
-	context.subscriptions.push(disposable);
-
-	// Add the command for jumping.
-	disposable = vscode.commands.registerCommand("pandoc-slides.jumpToSlide", (sourcepos) => {
-		jumpToSlide(context, sourcepos);
-	});
-	context.subscriptions.push(disposable);
+	// Create a slide preview instance and register all functionality on that instance. This means
+	// we don't need to manage singletons.
+	const slidePreviewPanel = new SlidePreviewPanel(context);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("pandoc-slides.sidePreview", slidePreviewPanel.showPreview.bind(slidePreviewPanel)),
+		vscode.commands.registerCommand("pandoc-slides.jumpToSlide", slidePreviewPanel.jumpToSlide.bind(slidePreviewPanel)),
+		vscode.languages.registerCodeLensProvider({
+			scheme: "file",
+			language: "markdown",
+		}, slidePreviewPanel.codeLensProvider),
+	);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
 
 class CodeLensProvider implements vscode.CodeLensProvider {
-	onDidChangeCodeLenses?: vscode.Event<void> | undefined;
+	private readonly _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+	public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
+	private _documentUri: vscode.Uri | undefined;
+	private _records: Array<any> | undefined;
+
+	public updateCodeLenses(documentUri: vscode.Uri, records: Array<any>) {
+		this._documentUri = documentUri;
+		this._records = records;
+		this._onDidChangeCodeLenses.fire();
+	}
+
 	provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
-		// Skip if there's no paired editor or the editor is for a different file.
-		let editor = SlidePreviewPanel.getInstance().pairedEditor;
-		if (!editor || editor.document.uri !== document.uri) {
+		// Skip if this is the wrong document.
+		if (document.uri !== this._documentUri) {
 			return [];
 		}
-		return SlidePreviewPanel.getInstance().sourceposLookup.map((message: any) => {
-			return new vscode.CodeLens(parseRange(message.sourcepos), {
-				title: "Jump to slide",
-				command: "pandoc-slides.jumpToSlide",
-				arguments: [message],
+		return this._records!.map((record: any) => {
+			return new vscode.CodeLens(parseRange(record.sourcepos), {
+				"title": "Jump to slide",
+				"command": "pandoc-slides.jumpToSlide",
+				"arguments": [record],
 			});
 		});
 	}
 
 	resolveCodeLens?(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens> {
-		throw new Error('Method not implemented.');
+		return codeLens;
 	}
 }
 
@@ -69,21 +70,16 @@ function parseRange(value: any) {
  * Singleton preview panel for slides.
  */
 class SlidePreviewPanel {
-	private static _instance: SlidePreviewPanel | undefined;
+	private readonly _context: vscode.ExtensionContext;
+	public readonly codeLensProvider: CodeLensProvider;
 	private _panel: vscode.WebviewPanel | undefined;
 	private _indexh: number = 0;
 	private _indexv: number = 0;
-	public pairedEditor: vscode.TextEditor | undefined;
-	public sourceposLookup: Array<any> = [];
+	private _pairedEditor: vscode.TextEditor | undefined;
 
-	/**
-	 * Get the singleton instance.
-	 */
-	public static getInstance() {
-		if (!SlidePreviewPanel._instance) {
-			SlidePreviewPanel._instance = new SlidePreviewPanel();
-		}
-		return SlidePreviewPanel._instance;
+	constructor(context: vscode.ExtensionContext) {
+		this._context = context;
+		this.codeLensProvider = new CodeLensProvider();
 	}
 
 	public navigate(indexh: number, indexv: number) {
@@ -93,35 +89,56 @@ class SlidePreviewPanel {
 		});
 	}
 
-	private constructor() {}
-
 	private _handleWebviewMessage(message: any) {
 		// Handle the `slidechanged` event and store the indices so we can jump straight back to the
 		// original slide when the preview is refreshed.
 		if (message.type === "slidechanged") {
 			this._indexh = message.indexh;
 			this._indexv = message.indexv;
+		// Handle a navigation event initiated by the slide view so we can look up the corresponding
+		// source.
 		} else if (message.type === "sourcepos-navigate") {
-			const range = parseRange(message.value);
-			this.pairedEditor!.selection = new vscode.Selection(range.start, range.end);
-			this.pairedEditor?.revealRange(range);
+			if (this._pairedEditor) {
+				const range = parseRange(message.value);
+				this._pairedEditor.selection = new vscode.Selection(range.start, range.end);
+				this._pairedEditor.revealRange(range);
+			}
+		// Handle information provided by the slide view so we can navigate from source to the
+		// corresponding slide.
 		} else if (message.type === "sourcepos") {
-			this.sourceposLookup = message.records;
+			if(this._pairedEditor) {
+				this.codeLensProvider.updateCodeLenses(this._pairedEditor.document.uri, message.records);
+			}
+		// Log any messages we don't know about for debugging.
 		} else {
 			console.log(`received unexpected message ${JSON.stringify(message)}`);
 		}
 	}
 
-	public async showPreview(context: vscode.ExtensionContext) {
-		// Reset any state if the uri of the file to be previewed has changed.
-		if (this.pairedEditor && this.pairedEditor!.document.uri !== vscode.window.activeTextEditor!.document.uri) {
+	public async jumpToSlide(record: any) {
+		// Post a message to navigate back to the slide we were on.
+		if (this._panel) {
+			this._panel.webview.postMessage({
+				"method": "slide",
+				"args": [record.indexh, record.indexv],
+			});
+		}
+	}
+
+	public async showPreview() {
+		// Abort if there is no active editor.
+		if (!vscode.window.activeTextEditor) {
+			return;
+		}
+
+		// Reset any state if the uri of the file to be previewed has changed and pair the editor.
+		if (this._pairedEditor && this._pairedEditor.document.uri !== vscode.window.activeTextEditor.document.uri) {
 			this._indexh = this._indexv = 0;
 		}
-		// Store the paired editor, update the title, html, and show the document.
-		this.pairedEditor = vscode.window.activeTextEditor;
-		// Create the preview panel if it does not exist and destroy it when the webview is disposed
-		// off, e.g., by the user closing the panel.
-		const title = `Preview ${path.basename(this.pairedEditor!.document.fileName)}`;
+		this._pairedEditor = vscode.window.activeTextEditor;
+
+		// Create the preview panel if it does not exist.
+		const title = `Preview ${path.basename(this._pairedEditor.document.fileName)}`;
 		if (!this._panel) {
 			this._panel = vscode.window.createWebviewPanel(
 				"slidePreview", title, vscode.ViewColumn.Two, {
@@ -129,18 +146,18 @@ class SlidePreviewPanel {
 					retainContextWhenHidden: true,
 			});
 			this._panel.iconPath = {
-				light: vscode.Uri.joinPath(context.extensionUri, "assets", "versions-light.svg"),
-				dark: vscode.Uri.joinPath(context.extensionUri, "assets", "versions-dark.svg"),
+				light: vscode.Uri.joinPath(this._context.extensionUri, "assets", "versions-light.svg"),
+				dark: vscode.Uri.joinPath(this._context.extensionUri, "assets", "versions-dark.svg"),
 			};
 			this._panel.onDidDispose(() => {
-				SlidePreviewPanel._instance = undefined;
+				this._panel = undefined;
 			});
 			this._panel.webview.onDidReceiveMessage(this._handleWebviewMessage.bind(this));
 		} else {
 			this._panel.title = title;
 		}
 
-		this._panel.webview.html = await this._getHtmlContent(context);
+		this._panel.webview.html = await this._getHtmlContent();
 		this._panel.reveal();
 
 		// Post a message to navigate back to the slide we were on.
@@ -150,10 +167,10 @@ class SlidePreviewPanel {
 		});
 	}
 
-	private async _getHtmlContent(context: vscode.ExtensionContext) {
+	private async _getHtmlContent() {
 		// Load the frontmatter and set defaults. We'll be loading the active file and writing to
 		// stdout.
-		const document = this.pairedEditor!.document;
+		const document = this._pairedEditor!.document;
 		let frontmatter = yamlfrontmatter.loadFront(document.getText());
 		let pandoc = frontmatter.pandoc ?? {};
 		pandoc["input-file"] = document.fileName;
@@ -168,7 +185,7 @@ class SlidePreviewPanel {
 		const documentUri = vscode.Uri.file(path.dirname(document.fileName));
 		pandoc.variables["header-includes"].push(`<meta name="document-webview-uri" content="${this._panel!.webview.asWebviewUri(documentUri)}/">`);
 		// Push the plugin code.
-		const pluginUri = vscode.Uri.joinPath(context.extensionUri, "assets", "plugin.js");
+		const pluginUri = vscode.Uri.joinPath(this._context.extensionUri, "assets", "plugin.js");
 		pandoc.variables["header-includes"].push(`<script src="${this._panel!.webview.asWebviewUri(pluginUri)}"></script>`);
 		// Push a random value so the webview content is reloaded.
 		pandoc.variables["header-includes"].push(`<meta name="webview-uuid" content="${uuid.v4()}">`);
@@ -190,7 +207,7 @@ class SlidePreviewPanel {
 				return stdout;
 			} catch (error: any) {
 				vscode.window.showErrorMessage(`Failed to compile slides: ${error.stderr}.`);
-				const errorUri = vscode.Uri.joinPath(context.extensionUri, "assets",
+				const errorUri = vscode.Uri.joinPath(this._context.extensionUri, "assets",
 					vscode.window.activeColorTheme.kind === vscode.ColorThemeKind.Light ?
 					"error-light.svg" : "error-dark.svg");
 				return `
@@ -201,12 +218,4 @@ class SlidePreviewPanel {
 		}, {postfix: ".yaml"});
 		return html;
 	}
-}
-
-async function showSidePreview(context: vscode.ExtensionContext) {
-	await SlidePreviewPanel.getInstance().showPreview(context);
-}
-
-async function jumpToSlide(context: vscode.ExtensionContext, sourcepos: any) {
-	SlidePreviewPanel.getInstance().navigate(sourcepos.indexh, sourcepos.indexv);
 }
